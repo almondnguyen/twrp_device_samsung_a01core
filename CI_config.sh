3d51b242263c760f07c3d363286799f630f8a2f8
Almond <42428221+almondnguyen@users.noreply.github.com>
2022-06-27 17:31:33 +0700

    CI: fix missing folder
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- CI_config.sh
+++ CI_config.sh
@@ -22,6 +22,7 @@ export USE_CCACHE=1
 export CCACHE_SIZE="50G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
+mkdir -p $SYNC_PATH
 
 if [ ! -z "$PLATFORM" ]; then
     export KERNEL_PATH="kernel/$OEM/$PLATFORM"
