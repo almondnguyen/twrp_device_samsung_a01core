af0485c3d2f1598664747a47f291cda6813ec419
Almond <42428221+almondnguyen@users.noreply.github.com>
2022-08-06 19:35:20 +0700

    a01core: config: oopsie number 2
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- omni_a01core.mk
+++ omni_a01core.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/samsung/a01core
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
