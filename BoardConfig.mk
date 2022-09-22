963e096eeec103fc64cca5e713887ac5071cb27a
Almond <42428221+almondnguyen@users.noreply.github.com>
2022-09-22 17:34:59 +0700

    twrp: this device is notchless
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,8 +132,6 @@ TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 
-TW_Y_OFFSET := 80
-TW_H_OFFSET := -80
 TW_THEME := portrait_hdpi
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
 TW_MAX_BRIGHTNESS := 240
