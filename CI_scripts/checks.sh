8280828ecf3d9eda3def602ae37f620c62f08b01
Almond <42428221+almondnguyen@users.noreply.github.com>
2023-06-25 22:01:25 +0700

    CI: so long, cirrus
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- CI_scripts/checks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Do not allow curl
-curl_check=$(grep 'curl ' $CONFIG | wc -l)
-if [[ $curl_check -gt 0 ]]; then
-    echo -e "Please dont use \'curl\' in $CONFIG".
-    exit 1
-fi
--- CI_scripts/upload.sh
+++
@@ -1,7 +0,0 @@
-source $CONFIG
-cd ~
-cd $SYNC_PATH
-
-cd out/target/product/${DEVICE}
-curl -sL $OUTFILE https://git.io/file-transfer | sh
-./transfer wet recovery.tar
