c1f55c9fdb1ab417edeed55a303ba5db2d8e59bc
Almond <42428221+almondnguyen@users.noreply.github.com>
2023-12-15 20:09:08 +0700

    BoardConfig: arm64 build
    Signed-off-by: Almond <42428221+almondnguyen@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,7 @@
 #
 
 DEVICE_PATH := device/samsung/a01core
+TARGET_BUILD_64BIT := true
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -28,12 +29,30 @@ TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 # Architecture
+ifeq ($(TARGET_BUILD_64BIT), true)
+# Build 64-bit TWRP
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+else
+# Build 32-bit TWRP
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_VARIANT_RUNTIME := cortex-a53
+endif # TARGET_BUILD_64BIT=true
 
 TARGET_BOARD_PLATFORM := mt6739
 TARGET_USES_64_BIT_BINDER := true
